814419c390efddfc30e50cc89c9d611e3a4af4eb
Heudiini <77001762+Heudiini@users.noreply.github.com>
2025-03-15 20:07:52 +0000

    alkuperöäinen toimiiko?

--- api/news.js
+++ api/news.js
@@ -1,16 +1,49 @@
-const axios = require("axios");
-
-// Lisää uutislähteet
-const sources = [
-  "https://www.tenerifenews.com/",
-  "https://planetacanario.com/",
-  "https://www.eldia.es/tenerife/",
-  "https://diariodeavisos.elespanol.com/",
-  "https://www.euroweeklynews.com/",
-  "https://www.reuters.com/",
+const newspapers = [
+  {
+    title: "10 Must-Try Restaurants in Santa Cruz de Tenerife",
+    url: "https://www.tenerifenews.com/restaurants-in-santa-cruz-de-tenerife/",
+    image:
+      "https://www.tenerifenews.com/wp-content/uploads/2024/09/restaurants-in-santa-cruz-de-tenerife.jpg",
+    source: "tenerife-news",
+    date: "2024-03-14", // YYYY-MM-DD
+  },
+  {
+    title: "Spain Tightens Rules on Holiday Rentals in Barcelona and Tenerife",
+    url: "https://www.tenerifenews.com/spain-tightens-rules-on-holiday-rentals/",
+    image:
+      "https://www.tenerifenews.com/wp-content/uploads/2024/09/New-rules-for-Spanish-rentals.jpg",
+    source: "tenerife-news",
+    date: "2024-03-12",
+  },
+  {
+    title:
+      "Tenerife necesitaría 20 veces más cantidad de energías limpias para autoabastecerse con renovables",
+    url: "https://planetacanario.com/tenerife-tendria-que-multiplicar-por-20-su-potencia-de-energias-limpias-para-ser-100-renovable/",
+    image:
+      "https://planetacanario.b-cdn.net/wp-content/themes/jnews/assets/img/jeg-empty.png",
+    source: "planeta-canario",
+    date: "2024-03-10",
+  },
+  {
+    title:
+      "Los cabildos de Tenerife y Gran Canaria garantizan la gratuidad del transporte público en 2025",
+    url: "https://planetacanario.com/los-cabildos-de-tenerife-y-gran-canaria-garantizan-la-gratuidad-del-transporte-publico-en-2025/",
+    image:
+      "https://planetacanario.b-cdn.net/wp-content/themes/jnews/assets/img/jeg-empty.png",
+    source: "planeta-canario",
+    date: "2024-03-08",
+  },
+  {
+    title: "Maná y Residente actuarán en junio en el ‘Tenerife Music Festival’",
+    url: "https://planetacanario.com/mana-y-residente-actuaran-en-junio-en-el-tenerife-music-festival/",
+    image:
+      "https://planetacanario.b-cdn.net/wp-content/themes/jnews/assets/img/jeg-empty.png",
+    source: "planeta-canario",
+    date: "2024-03-06",
+  },
 ];
 
-module.exports = async (req, res) => {
+module.exports = (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
@@ -19,42 +52,5 @@ module.exports = async (req, res) => {
     return res.status(200).end();
   }
 
-  // Hae sivunumero ja limit parametrit (oletuksena 1. sivu ja 2 uutista per sivu)
-  const page = parseInt(req.query.page) || 1;
-  const limit = parseInt(req.query.limit) || 2;
-  const searchQuery = req.query.search || "Tenerife"; // Käytetään hakusanaa (oletus: "Tenerife")
-
-  // Laske aloitusindeksi ja loppuindeksi
-  const startIndex = (page - 1) * limit;
-  const endIndex = startIndex + limit;
-
-  // Luo lista uutisista
-  const allNews = [];
-
-  try {
-    for (let source of sources) {
-      // Hae uutiset jokaiselta lähteeltä
-      const response = await axios.get(`${source}?search=${searchQuery}`);
-      const articles = response.data.articles;
-
-      // Lisää uutiset allNews-taulukkoon
-      allNews.push(...articles);
-    }
-
-    // Rajataan uutiset paginationin mukaan
-    const paginatedNews = allNews.slice(startIndex, endIndex);
-
-    res.status(200).json({
-      page,
-      limit,
-      total: allNews.length,
-      totalPages: Math.ceil(allNews.length / limit),
-      data: paginatedNews,
-    });
-  } catch (error) {
-    res.status(500).json({
-      error: "Uutisten hakeminen epäonnistui.",
-      details: error.message,
-    });
-  }
+  res.status(200).json(newspapers);
 };
